5225139eae872095ceec116acf38847df19f3683
Luke DUSSART <lukedussart@hotmail.fr>
2021-02-24 15:26:51 +0100

    footer icons

--- includes/navbar.php
+++ includes/navbar.php
@@ -31,6 +31,7 @@ $pageActuelle = substr($scriptName, strrpos($scriptName, '/') + 1);
           rel="stylesheet">
     <link href="https://kit-free.fontawesome.com/releases/latest/css/free.min.css" media="all" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.8.2/devicon.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/ionicons@4.4.6/dist/css/ionicons.min.css">
    <link rel="stylesheet" href="/css/style.css">
    <link rel="stylesheet" href="/css/styleGen.css">
 
