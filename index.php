7acd88943a02254fc1f576c781542b879df9c113
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-07 21:39:38 +0200

    ajout passiosn page index

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ include('./includes/navbar.php');
 <div class="container">
   <!-- Contenu -->
   <div class="presentation">
-    <div class="col-5">
+    <div class="col-4">
       <img class="img-fluid img-profile rounded-circle mx-auto mb-2 image" src="../images/dussartluke.jpg" alt="Image d'un jeune stressé et beau ">
     </div>
-    <div class="col-5">
+    <div class="col-6">
       <div class="group">
         <h2 class="title">
           <div>
@@ -37,26 +37,51 @@ include('./includes/navbar.php');
   </div>
 
   <div class="presentation2">
-    <div class="col-5">
+    <div class="col-6">
       <img class="img-fluid img-profile rounded-circle mx-auto mb-2 image1" src="../images/pic_ossau.jpg" alt="Image d'un jeune stressé et beau ">
     </div>
-    <div class="col-5">
-      <div class="group1">
-        <h2 class="title1">
+    <div class="col-6">
+      <div class="group">
+        <h2 class="title">
           <p>Présentation</p>
         </h2>
-        <div class="">
-          <p class="size-normal"> Je m'appelle Luke DUSSART et je suis actuellement en 2ème année de BTS SIO en option SLAM (solutions
-            logicielles et applications métiers).
-            Âgé de 21 ans, je suis un passionné de sport et d'informatique.
-          </p>
-          <p><br></p>
-          <p class="size-normal"></p>
-        </div>
+        <p class="size-normal"> Je m'appelle Luke DUSSART et je suis actuellement en 2ème année de BTS SIO en option SLAM (solutions
+          logicielles et applications métiers).
+          Âgé de 21 ans, je suis un passionné de sport et d'informatique.
+        </p>
       </div>
     </div>
   </div>
-</div>
+  <div class="presentation">
+    <div class="col-4">
+      <img class="img-fluid img-profile rounded-circle mx-auto mb-2 image" src="../images/dussartluke.jpg" alt="Image d'un jeune stressé et beau ">
+    </div>
+    <div class="col-6">
+      <div class="group">
+        <h2 class="title">
+          <p>Sport</p>
+        </h2>
+        <p class="size-normal"> Grand passionné de sport, j'ai longtemps pratiqué le football (13 ans) depuis mon plus jeune âge, puis depuis 3 ans,
+          je me suis mis à la musculation afin d'accroître ma confiance en moi ainsi que de me donner de nouveaux objectifs.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="presentation2">
+    <div class="col-6">
+      <img class="img-fluid img-profile rounded-circle mx-auto mb-2 image1" src="../images/pic_ossau.jpg" alt="Image d'un jeune stressé et beau ">
+    </div>
+    <div class="col-6">
+      <div class="group">
+        <h2 class="title">
+          <p>Jeux-vidéos</p>
+        </h2>
+        <p class="size-normal"> Depuis mon enfance, je me suis intéressé aux jeux-vidéos notamment ceux de sport ou de courses,
+          comme FIFA ou Gran Turismo. Je joue assez souvent soit tout seul, soit entre amis (en ligne ou non).
+        </p>
+      </div>
+    </div>
+  </div>
 </div>
 <!-- <section class="container content">
   <div class="container-fluid">
